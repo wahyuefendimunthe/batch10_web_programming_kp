5699e5342e18ecc47e60b9be807df29903ded8ab
Wahyu Efendi <140825542+wahyuefendimunthe@users.noreply.github.com>
2023-08-08 17:03:14 +0700

    Add files via upload

---
+++ INPG-PKJ-10-007_Wahyu Efendi M_Tugas Sesi 6/sesi6/dom.js
@@ -0,0 +1,12 @@
+document.write("Hello World YAL");
+document.write("<h2>Saya Sedang Belajar DOM</h2>");
+
+
+// mengakses objek body
+document.head;
+
+// mengakses objek body
+document.body;
+
+// melihat jumlah huruf di judul pada objek title
+document.title.length;
